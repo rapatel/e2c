98d94ff2192833edb909f9b6bbb3d5ed197a75f3
Ritesh Patel <ritesh88@gmail.com>
2012-01-21 15:17:16 -0800

    Initial testing of bcGen() function for boundary checking.

--- src/Parser.java
+++ src/Parser.java
@@ -28,8 +28,10 @@ public class Parser {
     }
 
     private Scan scanner;
+    private boolean bc;
     Parser(Scan scanner) {
         this.scanner = scanner;
+        bc = false;
         scan();
         program();
         if( tok.kind != TK.EOF )
@@ -39,7 +41,7 @@ public class Parser {
     // for code generation
     private static final int initialValueEVariable = 8888;
     private static final int initialValueEVariableArr = 4444;
-
+    
     // print something in the generated code
     private void gcprint(String str) {
         System.out.println(str);
@@ -54,6 +56,9 @@ public class Parser {
         gcprint("#include <stdio.h>");
         gcprint("main() ");
         block();
+        if(bc) {
+        	bcGen();
+        }
     }
 
     private void block() {
@@ -68,6 +73,18 @@ public class Parser {
         symtab.end_st_block();
         gcprint("}");
      }
+    
+    private void bcGen() {
+    	gcprint("int bc(int x, int size)");
+    	gcprint("{");
+    	gcprint("if(x<size && x>=0) {");
+    	gcprint("return x;");
+    	gcprint("} else {");
+    	gcprint("fprintf(stderr, \"Out of bounds.\\n\");");
+    	gcprint("exit(1);");
+    	gcprint("}");
+    	gcprint("}");
+    }
 
     private void declaration() {
         if (first(f_var_decl)) {
@@ -205,28 +222,7 @@ public class Parser {
     private void assignment(){
         if( is(TK.ID) ) {
             Entry e = lvalue_id(tok.string, tok.lineNumber);
-            
-            // TODO: Make function
             array(e);
-            /*if(e.arrSize>0 && tok.arr) {
-            	gcprint("[");
-            	gcprint("-("+ e.lbound +")+");
-            	scan();
-            	expression();
-            	if( is(TK.ENDARR) ) {
-            		gcprint("]");
-            	}
-            	else {
-            		parse_error("forgot closed bracket in array assignment");
-            	}
-            }
-            else if(tok.arr) {
-            	parse_error("using id as an array, but never was declared as an array");
-            }
-            else if(e.arrSize>0) {
-            	parse_error("declared id as an array, but not referencing as an array");
-            }*/
-            
             scan();
         }
         else {
@@ -379,25 +375,7 @@ public class Parser {
         }
         else if( is(TK.ID) ) {
             Entry e = rvalue_id(tok.string, tok.lineNumber);
-            
-			// TODO: Make function
             array(e);
-			/*if (e.arrSize > 0 && tok.arr) {
-				gcprint("[");
-				gcprint("-("+ e.lbound +")+");
-				scan();
-				expression();
-				if (is(TK.ENDARR)) {
-					gcprint("]");
-				} else {
-					parse_error("forgot closed bracket in array assignment");
-				}
-			} else if (tok.arr) {
-				parse_error("using id as an array, but never was declared as an array");
-			} else if (e.arrSize > 0) {
-				parse_error("declared id as an array, but not referencing as an array");
-			}*/
-            
             scan();
         }
         else if( is(TK.NUM) ) {
@@ -410,11 +388,16 @@ public class Parser {
     
     private void array(Entry e) {
     	if (e.arrSize > 0 && tok.arr) {
+    		bc = true;
 			gcprint("[");
+			gcprint("bc(");
 			gcprint("-("+ e.lbound +")+");
 			scan();
 			expression();
 			if (is(TK.ENDARR)) {
+				gcprint(",");
+				gcprint(e.arrSize+"");
+				gcprint(")");
 				gcprint("]");
 			} else {
 				parse_error("forgot closed bracket in array assignment");
